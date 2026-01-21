8b4b620d749afb47020be8ccb665a6b3bf1f7e9c
Fabian Rausch <fabian.rausch@pwc.com>
2026-01-22 00:21:23 +0100

    Adjust interval and decrease mobile paddings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     };
 
     fetchDepartures();
-    const interval = setInterval(fetchDepartures, 60000);
+    const interval = setInterval(fetchDepartures, 20000);
 
     return () => clearInterval(interval);
   }, []);
@@ -34,7 +34,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center px-8 py-8 justify-between bg-white dark:bg-black sm:items-start">
         <DepartureList departures={departures} />
       </main>
     </div>
